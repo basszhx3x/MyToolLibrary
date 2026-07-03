6d322f380b353fcf65fb1cb623cf5676c524e97a
basszhx3x <basszhx3x@163.com>
2026-07-03 09:50:26 +0800

    chore: remove ToolLibrary test project

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/evgenyneu/keychain-swift.git", .upToNextMajor(from: "20.0.0")),
+        .package(url: "https://github.com/evgenyneu/keychain-swift.git", .upToNextMajor(from: "24.0.0")),
         .package(url: "https://github.com/iAmMccc/SmartCodable.git", branch: "main"),
         .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.6.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
